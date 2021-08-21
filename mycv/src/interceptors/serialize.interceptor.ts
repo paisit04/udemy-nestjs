044ec38bd60d7b12bdaa645dd51d134b669a4a5d
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2021-08-20 17:21:17 -0700

    Section 11: Authentication From Scratch

--- mycv/src/interceptors/serialize.interceptor.ts
+++ mycv/src/interceptors/serialize.interceptor.ts
@@ -22,12 +22,12 @@ export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, handler: CallHandler): Observable<any> {
     // Run something before a request is handled
     // by the request handler
-    console.log('Im running before the handler', context);
+    // console.log('Im running before the handler', context);
 
     return handler.handle().pipe(
       map((data: any) => {
         // Run something before the response is sent out
-        console.log('Im running before the response is sent out', data);
+        // console.log('Im running before the response is sent out', data);
 
         return plainToClass(this.dto, data, {
           excludeExtraneousValues: true
