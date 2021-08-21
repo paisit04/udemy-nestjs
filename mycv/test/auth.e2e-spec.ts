5108f942d4b3a20168cf38f784668cde46e7a345
Paisit Jarunnamsiri <paisitj@MacBook-Pro.local>
2021-08-21 02:48:28 -0700

    Section 14: Managing App Configuration

--- mycv/test/auth.e2e-spec.ts
+++ mycv/test/auth.e2e-spec.ts
@@ -21,7 +21,7 @@ describe('Authentication System', () => {
       .post('/auth/signup')
       .send({
         email,
-        password: 'asdf'
+        password: 'asdf',
       })
       .expect(201)
       .then((res) => {
@@ -29,5 +29,24 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a new user then get the currently logged in user', async () => {
+    const email = 'asdf@asdf.com';
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({
+        email,
+        password: 'asdf',
+      })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+    const {body} = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
